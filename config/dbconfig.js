26cc317dfeeafe2903444a6c75af61652f8b4587
FadelKachour <fadel.kachour@estiam.com>
2021-07-08 00:34:51 +0200

    mongo update v2

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,3 +1,3 @@
 module.exports ={
-    url:"mongodb://localhost:27017/school_db"
+    url:"mongodb://gamifyback:smTJg5KFsVkUFYsi3I0TDZS52UtclNGbIUIY6WvoSbDYKKozMSSrAM8CRIRJsmQqlFHSmJKfAV4UNI68mTQ7fQ==@gamifyback.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@gamifyback@"
 }
